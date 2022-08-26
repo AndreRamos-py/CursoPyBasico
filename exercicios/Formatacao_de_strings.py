e6c88244bd180cc736aed09953c644a8cee13786
AndreRamos-py <83097746+AndreRamos-py@users.noreply.github.com>
2022-08-25 23:26:21 -0300

    Aula Formatação de Strings
    Exercício de formatações de strings e algumas anotações.

---
+++ exercicios/Formatacao_de_strings.py
@@ -0,0 +1,8 @@
+nome = 'Eunice'
+idade = 51
+altura = 1.60
+peso = 55.0
+imc = peso / (altura ** 2)
+ano_atual = 2022
+ano = ano_atual - idade
+print(f'{nome} tem {idade} anos nasceu em {ano}, {nome} tem {peso}kg e seu IMC é {imc:.2f}')
