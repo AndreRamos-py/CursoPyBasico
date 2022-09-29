4e96b5b71455a8aab583a782f8db3e97501b3df3
AndreRamos-py <83097746+AndreRamos-py@users.noreply.github.com>
2022-09-29 18:29:53 -0300

    Exercícios com while
    Algumas anotações sobre o funcionamento do laço while e exercícios.

---
+++ exercicios/calculadora_simples.py
@@ -0,0 +1,26 @@
+while True:
+    num_1 = input('Digite um número: ')
+    num_2 = input('Digite outro número: ')
+    operador = input('Digite um operador: ')
+    sair = input('Deseja sair? sim/não ')
+
+    if sair == 'sim':
+        break
+
+    if not num_1.isnumeric() or not num_2.isnumeric():
+        print('Você deve digitar uma número!')
+        continue
+
+    num_1 = int(num_1)
+    num_2 = int(num_2)
+
+    if operador == '+':
+        print(num_1 + num_2)
+    elif operador == '-':
+        print(num_1 - num_2)
+    elif operador == '*':
+        print(num_1 * num_2)
+    elif operador == '/':
+        print(num_1 / num_2)
+    else:
+        print('Operador Inválido!')
---
+++ exercicios/exercicios_while.py
@@ -0,0 +1,9 @@
+x = 0
+while x < 10:
+    y = 0
+
+    while y < 5:
+        print(f'X vale {x},Y vale {y}')
+        y += 1
+    x += 1
+print('Acabou!')
