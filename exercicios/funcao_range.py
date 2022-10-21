b8dbc12edc8a470b0e3cf8e92a8b4d584c306c6d
AndreRamos-py <83097746+AndreRamos-py@users.noreply.github.com>
2022-10-21 12:04:20 -0300

    Sobre range
    Aprendendo sobre range e incluindo anotação.

---
+++ exercicios/funcao_range.py
@@ -0,0 +1,20 @@
+
+#for n in range(10):
+#    print(n)
+
+#Outro exemplo#
+
+#for n in range(5, 15, 2):
+#    print(n)
+
+texto = 'Python'
+nova_str = ''
+
+for letra in texto:
+    if letra == 't':
+        nova_str = nova_str + letra.upper()
+    elif letra == 'h':
+        nova_str = nova_str + letra.upper()
+    else:
+        nova_str += letra
+print(nova_str)
